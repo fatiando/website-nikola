534dd60ebf90f9c60646ea715ccbf1c0e3515e5a
Leonardo Uieda <leouieda@gmail.com>
2013-02-15 15:56:21 -0200

    Added CCBY license notice to footer

--- conf.py
+++ conf.py
@@ -246,7 +246,7 @@ INDEX_TEASERS = True
 
 # A small copyright notice for the page footer (in HTML).
 # Default is ''
-CONTENT_FOOTER = 'Contents &copy; {date} <a href="http://fatiando.org/people/uieda">{author}</a> - Powered by <a href="http://nikola.ralsina.com.ar">Nikola</a>'
+CONTENT_FOOTER = '<br>Contents &copy; {date} <a href="/people/uieda/index.html">{author}</a> - Powered by <a href="http://nikola.ralsina.com.ar">Nikola</a> <br>  Except where otherwise noted, all content is avilable under a <a href="http://creativecommons.org/licenses/by/3.0/legalcode">CC-BY license</a>. <br><a href="http://creativecommons.org/licenses/by/3.0/legalcode"><img src="/logo/cc-by.jpg" width="90px" style="padding-top:5px"></a>' 
 CONTENT_FOOTER = CONTENT_FOOTER.format(email=BLOG_EMAIL,
                                        author=BLOG_AUTHOR,
                                        date=time.gmtime().tm_year)
