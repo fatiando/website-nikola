a3b80ac6d440f62d3697d74b5d06d6ddcd41cd33
Leonardo Uieda <leouieda@gmail.com>
2013-02-28 15:11:02 -0300

    Added google analytics code to conf.py

--- conf.py
+++ conf.py
@@ -317,7 +317,21 @@ RSS_TEASERS = False
 
 # Google analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
-# ANALYTICS = ""
+ANALYTICS = """
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38125837-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+"""
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
